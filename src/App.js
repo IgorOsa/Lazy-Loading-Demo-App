b7a3988ec6aa5dfffe5466dbd69a56d913f44af6
Igor Osadchyi <igor@webio.net.ua>
2019-06-09 20:41:05 +0300

    LazyLoad with Bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import data from "./data";
+import {Container, Row, Media, Image, Spinner} from 'react-bootstrap';
+import LazyLoad from 'react-lazyload';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Loading = () => (
+  <ul className="list-unstyled">
+    <Media as="li">
+      <Media.Body>
+        <h4>Loading...</h4>
+      </Media.Body>
+    </Media>
+  </ul>
+)
+
+const Post = ({id, title, body, img}) => (
+  <ul className="list-unstyled">
+    <Media as="li">
+      <LazyLoad 
+        once={true} 
+        placeholder={<img src={`https://picsum.photos/id/${id}/5/5`}  alt="..." />}
+      >
+        <Image
+          src={`https://picsum.photos/id/${id}/200/200`} 
+          alt=''
+          width={200} 
+          height={200}
+          className="mr-3"
+          thumbnail
+        />
+      </LazyLoad>
+      <Media.Body>
+        <h3>{title}</h3>
+        <p>{body}</p>
+      </Media.Body>
+    </Media>
+  </ul>
+);
+
+const App = () => (
+  <div className="App">
+    <Container>
+      <Row>
+        <h2>LazyLoad Demo App</h2>
+      </Row>
+      <Row>
+        <div className="post-container">
+          {data.map(post => (
+            <LazyLoad 
+              key={post.id} 
+              height={100}
+              offset={[-100, 100]}
+              placeholder={<Spinner />}
+            >
+              <Post key={post.id} {...post} />
+            </LazyLoad>
+          ))}
+        </div>
+      </Row>
+    </Container>
+  </div>
+);
 
 export default App;
